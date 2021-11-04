1ad40ef367798f19a78e37d1c4f1c18d1260be51
Victor Martinez Jr <victormtzcodes@gmail.com>
2021-11-04 18:21:04 -0400

    move main arrow up

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,9 +11,11 @@ const Main = () => {
                     <h1 className='main-title'>Delicious Food</h1>
                     <p className='main-subtitle'>Crown is a restaurant, bar, and coffee<br></br>roastery located in Atlanta, Ga
                     </p>
-                    <button className='main-btn'>Order Online</button>
+                    <div className='arrow-container'>
+                        <button className='main-btn'>Order Online</button>
+                        <i className="fas fa-long-arrow-alt-down main-arrow"></i>
+                    </div>
                 </div>
-                <i className="fas fa-long-arrow-alt-down main-arrow"></i>
             </div>
         </section>
     )
